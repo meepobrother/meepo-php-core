c0a116178a18310e236ddc5edac1e0a441bb2773
15138730370 <1037483576@qq.com>
2018-02-09 09:00:59 +0800

    update to save

--- index.php
+++ index.php
@@ -4,25 +4,23 @@ ini_set('display_errors', true);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-use Rx\Observable;
 use React\EventLoop\Factory;
-use Rx\Scheduler;
+use Rx\Observable;
 
-$loop = Factory::create();
+try {
 
-//You only need to set the default scheduler once
-Scheduler::setDefaultFactory(function() use($loop){
-    return new Scheduler\EventLoopScheduler($loop);
-});
+    $loop = Factory::create();
 
-Observable::interval(1000)
-    ->take(5)
-    ->flatMap(function ($i) {
-        return Observable::of($i + 1);
-    })
-    ->subscribe(function ($e) {
-        echo $e, PHP_EOL;
-    });
+    Observable::interval(1000)
+        ->take(5)
+        ->flatMap(function ($i) {
+            return Observable::of($i + 1);
+        })
+        ->subscribe(function ($e) {
+            echo $e, PHP_EOL;
+        });
 
-$loop->run();
+    $loop->run();
+} catch (Exception $e) {
+    print_r($e);
+}
